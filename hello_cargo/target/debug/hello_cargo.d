f19e6ee092542c2b583a4cf1d2fe46c17b8234b8
Daniele Simonetti <daniele.simonetti@orpak.com>
2021-08-18 10:04:21 +0200

    add .gitignore, removed build artifacts

--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-C:\Code\rust_learning\hello_cargo\target\debug\hello_cargo.exe: C:\Code\rust_learning\hello_cargo\src\main.rs
--- hello_cargo/target/release/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-C:\Code\rust_learning\hello_cargo\target\release\deps\hello_cargo.exe: src\main.rs
-
-C:\Code\rust_learning\hello_cargo\target\release\deps\hello_cargo.d: src\main.rs
-
-src\main.rs:
